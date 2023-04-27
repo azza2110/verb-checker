105a27a0b53cef261e11aa5e3053dc5b22733589
Aaron Brooker <aaronbrooker2110@gmail.com>
2023-04-27 19:36:09 +0100

    Highlights blacklisted verbs in orange, removes when focused

---
+++ script.js
@@ -0,0 +1,60 @@
+//Development notes
+//Bonus 1: Clear button
+//Bonus 2: List out the verbs in a table beneath the card
+//Alternative version for listening for everytime text content changes
+    /*inputs.forEach(input => {
+        input.addEventListener('DOMCharacterDataModified', verbCheck);
+    });
+    function verbCheck(e) {
+        let editedInput = e.target.parentNode;
+        ...
+    }
+    */
+
+let verbBlackList = ["review", "support", "consider", "try", "continue", "continue to try"];
+
+let inputs = document.querySelectorAll(".textbox");
+
+inputs.forEach(input => {
+    input.addEventListener('focusout', verbCheck);
+    input.addEventListener('focusin', removeSpans);
+});
+
+function verbCheck(e) {
+    let editedInput = e.target;
+    editedInput.classList.add("edited");
+    fullString = editedInput.textContent;
+    allWords = fullString.split(" ");
+    let test1 = allWords[0];
+    let test2 = allWords[0]+" "+allWords[1];
+    let test3 = allWords[0]+" "+allWords[1]+" "+allWords[2];
+    verbBlackList.forEach(vagueVerb => {
+        if (vagueVerb == test3.toLowerCase()) {
+            addSpan(editedInput, test3, allWords, 3);
+        } else if (vagueVerb == test2.toLowerCase()) {
+            addSpan(editedInput, test2 ,allWords, 2);
+        } else if (vagueVerb == test1.toLowerCase()) {
+            addSpan(editedInput, test1, allWords, 1);
+        }
+    })
+}
+
+function addSpan(editedInput, matchedPhrase, allWords, matchLength) {
+    console.log(editedInput, matchedPhrase, allWords, matchLength);
+    let remainingWords = allWords.slice(matchLength);
+    let remainingString = " " + remainingWords.join(" ");
+    editedInput.textContent = remainingString;
+    let vagueSpan = document.createElement("span");
+    vagueSpan.classList.add("vague");
+    vagueSpan.textContent=matchedPhrase;
+    editedInput.prepend(vagueSpan);
+}
+
+function removeSpans(e) {
+    let focusedInput = e.target;
+    let existingString = focusedInput.textContent;
+    focusedInput.innerHTML = existingString;
+}
+
+
+
